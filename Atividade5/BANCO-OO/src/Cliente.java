0b00c2d96ae29a4ace1dab2ba6a6d6b36436cea3
vitoria <vitoriabf2006@gmail.com>
2025-10-23 09:33:24 -0300

    :sparkles: feat: adiciona lista de contas aos atributos da classe Cliente, métodos getters e setters e também um método de adicionar conta à lista de contas.

--- Atividade5/BANCO-OO/src/Cliente.java
+++ Atividade5/BANCO-OO/src/Cliente.java
@@ -1,16 +1,13 @@
-// import java.util.ArrayList;
-// import java.util.Date;
-// import java.util.List;
-
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-
 public class Cliente {
     private int id;
     private String nome;
     private String CPF;
     private LocalDate dataNasc;
-    //private List<Conta> contas = new ArrayList<>();
+    private List<Conta> contas = new ArrayList<>();
 
     static DateTimeFormatter formatoBR = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
@@ -21,6 +18,14 @@ public class Cliente {
         this.dataNasc = LocalDate.parse(dataNasc, formatoBR);
     }
 
+    public Cliente(int id, String nome, String CPF, String dataNasc, Conta conta) {
+        this.id = id;
+        this.nome = nome;
+        this.CPF = CPF;
+        this.dataNasc = LocalDate.parse(dataNasc, formatoBR);
+        this.contas.add(conta);
+    }
+
     public String dados(){
         return String.format(
         """
@@ -29,6 +34,10 @@ public class Cliente {
         | CPF: %s
         | DATA DE NASCIMENTO: %s
         """, this.id, this.nome, this.CPF, this.dataNasc.format(formatoBR));
+    }
+
+    public void adicionarConta(Conta conta){
+        this.contas.add(conta);
     }
 
     public int getId() {
@@ -54,4 +63,22 @@ public class Cliente {
     public void setCPF(String CPF) {
         this.CPF = CPF;
     }
+
+
+    public LocalDate getDataNasc() {
+        return this.dataNasc;
+    }
+
+    public void setDataNasc(LocalDate dataNasc) {
+        this.dataNasc = dataNasc;
+    }
+
+    public List<Conta> getContas() {
+        return this.contas;
+    }
+
+    public void setContas(List<Conta> contas) {
+        this.contas = contas;
+    }
+
 }
