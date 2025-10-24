1ce07c0f57481f48d73ecad14b2ef0ea2de16d58
vitoria <vitoriabf2006@gmail.com>
2025-10-24 19:34:40 -0300

    :sparkles:feat: adiciona funções de validação de entrada de dados nas funções cadastrarCliente e criarConta.

---
+++ Atividade5/BANCO-OO/src/utilitarios/Cadastro.java
@@ -0,0 +1,84 @@
+package utilitarios;
+
+import static utilitarios.Utils.*;
+import static utilitarios.EntradaDeDados.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import classes.Cliente;
+import classes.Conta;
+
+public class Cadastro {
+
+    public static Cliente cadastrarCliente(List<Cliente> clientes){
+        System.out.println("\n>>> CADASTRAR CLIENTE:\n");
+
+        int id = recebeInt("Id do Cliente:");
+        limparBuffer();
+        String nome = recebeStr("Nome:");
+        String cpf = recebeStr("CPF:");
+        LocalDate dataNasc = recebeData("Data de nascimento (DD/MM/AAAA): ");
+
+        if( isCPFValido(cpf, clientes) && isIdClienteValido(id, clientes)){
+            return new Cliente(id, nome, cpf, dataNasc);
+        }
+
+        System.out.println("Id ou CPF indisponível para uso. Tente novamente:");
+        return cadastrarCliente(clientes);
+    } 
+
+    public static Conta criarConta(Cliente cliente, List<Conta> contas){
+        System.out.println("\n>>> CRIAR CONTA:\n");
+
+        int idConta = recebeInt("Id da conta:");
+        limparBuffer();
+        String numero = recebeStr("Número da conta:");
+        LocalDate dataAbertura = recebeData("Data de abertura (DD/MM/AAAA): ");
+
+        if (isIdContaValida(idConta, contas) && isNumContaValido(numero, contas)){
+            return new Conta(idConta, numero, cliente, dataAbertura);
+        }
+        
+        System.out.println("Id ou número indisponível para uso.Tente novamente:");
+        return criarConta(cliente, contas);
+    }
+
+    public static boolean isIdClienteValido(int id, List<Cliente> clientes){
+        for(Cliente cliente : clientes){
+            if(cliente.getId() == id){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isIdContaValida(int id, List<Conta> contas){
+        for(Conta conta : contas){
+            if(conta.getIdConta() == id){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isCPFValido(String cpf, List<Cliente> clientes){
+        for(Cliente cliente : clientes){
+            if(cliente.getCPF().equals(cpf)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNumContaValido(String numero, List<Conta> contas){
+       for(Conta conta : contas){
+            if(conta.getNumero() == numero){
+                return false;
+            }
+        }
+        return true; 
+    }
+}
+
+
